9b74d85454a8813d5804107d9a5c590a67261481
Housni Mohamed <mohamed.housni@um5s.net.ma>
2022-09-10 13:36:32 +0100

    Create tri_a_bulle.c

---
+++ tri_a_bulle.c
@@ -0,0 +1,21 @@
+// tri à bulle
+#define TRUE 1
+#define FALSE 0
+void tri_a_bulle(int *t, int n)
+{
+ int j = 0;
+ int tmp = 0;
+ int en_desordre = 1;
+ while(en_desordre)
+ {
+ en_desordre = FALSE;
+   for(j = 0 ; j < n- 1 ; j++){
+    if(t[j] > t[j+1]){
+      tmp = t[j+1];
+      t[j+1] = t[j];
+      t[j] = tmp;
+       en_desordre = TRUE;
+    }
+   }
+ }
+}

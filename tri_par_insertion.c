5c3572a867b67fa1ba01b421fd449ae09228a909
Housni Mohamed <mohamed.housni@um5s.net.ma>
2022-09-10 13:39:07 +0100

    Create tri_par_insertion.c

---
+++ tri_par_insertion.c
@@ -0,0 +1,17 @@
+// tri par insertion
+void tri_insertion(int *t, int n)
+{
+ int i,p,j;
+ int x;
+ for (i = 1; i < n; i++)
+ {
+  x = t[i];
+  p = i-1;
+  while (t[p] > x && p-- > 0) {}
+  p++;
+  for (j = i-1; j >= p; j--) {
+   t[j+1] = t[j];
+  }
+  t[p] = x;
+ }
+}
